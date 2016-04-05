c9def9fea46c1a63be962a568b94cfa01c4ea70a
barath3981 <barath.baisetty@gmail.com>
2016-04-05 19:03:40 +0530

    Changed routes file and two more controllers

---
+++ static/js/controllers/courseManagement/coachingManagement.js
@@ -0,0 +1,13 @@
+/**
+ * Created by bhanu on 5/4/16.
+ */
+
+
+var app = angular.module('mainCollegeApp.coachingManagementApp', []);
+
+app.controller('coachingManagementCtrl', function($scope) {
+
+    console.log('Loaded coaching management controller.');
+
+});
+
